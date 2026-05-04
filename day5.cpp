7e32a3ffa6d30b26dfa16776d594c8a79fd63eb9
Divyansh Gupta <guptadivyansh2507@gmail.com>
2026-05-04 13:04:59 +0530

    Add logging functionality in day5.cpp

---
+++ day5.cpp
@@ -0,0 +1,16 @@
+#include <bits/stdc++.h>
+using namespace std;
+void Log(const char * message){
+  cout<<message<<endl;
+}
+int main(){
+  for (int i = 0;i< 5;i++){
+    Log("Hello world");
+  }
+  Log("===============");
+  int i=0;
+  while (i<5){
+    Log("Hello world");
+    i++;
+  }
+}   
