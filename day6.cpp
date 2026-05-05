3e4b77db5662412bee26e8f82780814edcef5433
Divyansh Gupta <guptadivyansh2507@gmail.com>
2026-05-05 17:20:36 +0530

    Add memory allocation for buffer in main function
    Allocate memory for a buffer and initialize it to zero.

---
+++ day6.cpp
@@ -0,0 +1,25 @@
+/*
+#include <bits/stdc++.h>
+using namespace std;
+void Log(const char * message){
+  cout<<message<<endl;
+}
+int main(){
+  for (int i = 0;i< 5;i++){
+    if((i+1)%2==0)
+    return 0;
+    Log("Hello world");
+    cout<<i<<endl;
+  }
+}   
+
+*/
+#include <bits/stdc++.h>
+using namespace std;
+void Log(const char * message){
+  cout<<message<<endl;
+}
+int main(){
+  char* buffer = new char[8];
+  memset(buffer, 0 , 8);
+}
